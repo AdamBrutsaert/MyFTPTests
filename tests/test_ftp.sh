469a808e1034420ebd153bb162c46bdd0c1360a3
Adam Brutsaert <brutsaertadam@yahoo.fr>
2024-02-22 15:15:52 +0100

    feat: added more tests for authentication and invalid commands

--- tests/test_ftp.sh
+++ tests/test_ftp.sh
@@ -10,7 +10,7 @@ source tests/test_lib.sh
 HOST=$1
 PORT=$2
 
-test00()
+test_auth_00()
 {
   local test_name="Authentication as Anonymous"
 
@@ -28,7 +28,7 @@ test00()
   return
 }
 
-test01()
+test_auth_01()
 {
   local test_name="Authentication with wrong username"
 
@@ -40,12 +40,13 @@ test01()
   fi
 
   launch_test "$test_name" "USER invalid" 331
+  launch_test "$test_name" "PASS" 530
 
   print_succeeded "$test_name"
   return
 }
 
-test02()
+test_auth_02()
 {
   local test_name="Authentication with wrong password"
 
@@ -63,6 +64,103 @@ test02()
   return
 }
 
-test00; clean
-test01; clean
-test02; clean
+test_auth_03()
+{
+  local test_name="Authentication with password first"
+
+  launch_client $HOST $PORT
+  if [[ ! $? -eq 1 ]]; then
+    echo "KO"
+    kill_client
+    return
+  fi
+
+  launch_test "$test_name" "PASS invalid" 503
+
+  print_succeeded "$test_name"
+  return
+}
+
+test_auth_04()
+{
+  local test_name="Can't authenticate another time"
+
+  launch_client $HOST $PORT
+  if [[ ! $? -eq 1 ]]; then
+    echo "KO"
+    kill_client
+    return
+  fi
+
+  launch_test "$test_name" "USER Anonymous" 331
+  launch_test "$test_name" "PASS" 230
+  launch_test "$test_name" "USER other" 530
+
+  print_succeeded "$test_name"
+  return
+}
+
+test_auth_05()
+{
+  local test_name="Pass after authentication"
+
+  launch_client $HOST $PORT
+  if [[ ! $? -eq 1 ]]; then
+    echo "KO"
+    kill_client
+    return
+  fi
+
+  launch_test "$test_name" "USER Anonymous" 331
+  launch_test "$test_name" "PASS" 230
+  launch_test "$test_name" "PASS" 230
+
+  print_succeeded "$test_name"
+  return
+
+}
+
+test_invalid_00()
+{
+  local test_name="Invalid command before authentication"
+
+  launch_client $HOST $PORT
+  if [[ ! $? -eq 1 ]]; then
+    echo "KO"
+    kill_client
+    return
+  fi
+
+  launch_test "$test_name" "INVALID" 530
+
+  print_succeeded "$test_name"
+  return
+}
+
+test_invalid_01()
+{
+  local test_name="Invalid command after authentication"
+
+  launch_client $HOST $PORT
+  if [[ ! $? -eq 1 ]]; then
+    echo "KO"
+    kill_client
+    return
+  fi
+
+  launch_test "$test_name" "USER Anonymous" 331
+  launch_test "$test_name" "PASS" 230
+  launch_test "$test_name" "INVALID" 500
+
+  print_succeeded "$test_name"
+  return
+}
+
+test_auth_00; clean
+test_auth_01; clean
+test_auth_02; clean
+test_auth_03; clean
+test_auth_04; clean
+test_auth_05; clean
+test_invalid_00; clean
+test_invalid_01; clean
